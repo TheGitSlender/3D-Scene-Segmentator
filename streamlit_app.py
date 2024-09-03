28ad0761dc1f44ec72c681160a838adc4863cf2a
TheSlender <105570080+TheGitSlender@users.noreply.github.com>
2024-09-03 20:33:39 +0000

    testing the output of streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,86 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-st.title("🎈 My new app")
+
+
+st.title("Superpoint Transformer")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write("The work presented in this web-app is an implementation of the model presented in the paper \"Scalable 3D Panoptic Segmentation As Superpoint Graph Clustering (3DV 2024 Oral). \"")
+
+st.write("I'm trying some things out right now.")
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title= 'SuperPoint Transformer',
+        menu_icon= 'image/page-icon.png',
+        options= ['🏠 Home', '📚 How To Use', '🛠️ Segmentation Tool'],
+        default_index= 0, 
+    )
+if selected == '🏠 Home':
+    
+    # Apply custom CSS styles for the dark theme and improved design
+    st.markdown(
+        """
+        <style>
+        /* Set the dark mode theme for the application */
+        body {
+            background-color: #121212;
+            color: #ffffff;
+            font-family: 'Segoe UI', 'Helvetica', 'Arial', sans-serif;
+        }
+        /* Header styles */
+        .header {
+            padding: 20px;
+            background-color: #1a535c;
+            color: #ffffff;
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        /* Logo styles */
+        .logo {
+            display: block;
+            margin: 0 auto;
+            width: 150px;
+            border-radius: 50%;
+        }
+        /* Content styles */
+        .content {
+
+            background-color: #333;
+            box-shadow: 0 4px 12px rgba(0, 0, 0, 0.3);
+            margin-bottom: 20px;
+        }
+        /* Styles for headings and text */
+        h1, h2 {
+            color: #4ecdc4;
+        }
+        /* Button styles */
+        .button {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #4ecdc4;
+            color: #121212;
+            font-size: 16px;
+            font-weight: bold;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s, color 0.3s;
+        }
+        .button:hover {
+            background-color: #2b7a78;
+            color: #ffffff;
+        }
+        /* Image styles */
+        .image {
+            width: 100%;
+            border-radius: 10px;
+            margin-top: 20px;
+            box-shadow: 0 4px 12px rgba(0, 0, 0, 0.5);
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
